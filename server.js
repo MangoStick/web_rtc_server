1bc5e0f468d5a071ac84f24d36cf74eb3d1e3d36
N.Jakkapat <30841151+MangoStick@users.noreply.github.com>
2022-09-12 12:50:05 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const app = require('express')();
+const {Server} = require('socket.io');
+const { v4: uuidv4 } = require("uuid");
+
+async function server(){
+    const http = require('http').createServer(app);
+    const io = new Server(http, {transports: ['websocket']});
+    const roomName = 'prem'  //uuidv4();
+    io.on('connection', (socket) => {
+
+        console.log('this is main socket ' + socket)
+
+        socket.on('join', (data) =>{
+            console.log('join ' + data)
+            var x = JSON.parse(data)
+            console.log('join ' + x.msg)
+            // console.log('join ' + uuidv4())
+            socket.join(x.msg);
+            socket.to(x.msg).emit('joined');
+
+            socket.on('disconnect', () => {
+                console.log('disconnect')
+                console.log('disconnect ' + x.msg)
+                // var x = JSON.parse(data)
+                // socket.to(x.msg).broadcast.emit('user-disconnected', x.msg)
+            })
+    
+       });
+       socket.on('offer', (offer) => {
+            console.log('offer ' + roomName)
+            var x = JSON.parse(offer)
+            console.log('offer ' + offer)
+           socket.to(x.msg).emit('offer', offer);
+        });
+        socket.on('answer', (answer) => {
+            console.log('answer ' + roomName)
+            var x = JSON.parse(answer)
+            console.log('answer ' + answer)
+            socket.to(x.msg).emit('answer', answer);
+        });
+        socket.on('ice', (ice) => {
+            console.log('ice ' + roomName)
+            var x = JSON.parse(ice)
+            console.log('ice ' + ice)
+            socket.to(x.msg).emit('ice', ice);
+        });
+
+    });
+    http.listen(3000, () => console.log('server open !!'));
+}
+
+server();
