e5aab5cf191394895d5eb1e70e77b869ada8b452
N.Jakkapat <30841151+MangoStick@users.noreply.github.com>
2022-09-12 22:37:53 +0700

    make it better

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server);
+const io = new Server(server, {transports: ['websocket']});
 const PORT = process.env.PORT || 3000;
 
 
