1c9075a8cd8270c974a765eb08c3f8bf494d8bb8
N.Jakkapat <30841151+MangoStick@users.noreply.github.com>
2022-09-13 16:58:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,6 @@
 // const {Server} = require('socket.io');
 // const { v4: uuidv4 } = require("uuid");
 
-
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server, {transports: ['websocket']});
-const PORT = process.env.PORT || 3000;
-
-
 // async function server(){
 //     const http = require('http').createServer(app);
 //     const io = new Server(http, {transports: ['websocket']});
@@ -62,6 +52,13 @@ const PORT = process.env.PORT || 3000;
 // server();
 
 
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server, {transports: ['websocket']});
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.write(`<h1>Socket IO Start on Port : ${PORT}</h1>`);
@@ -69,42 +66,32 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
 
-    console.log('this is main socket ' + socket)
-
     socket.on('join', (data) =>{
-        console.log('join ' + data)
-        var x = JSON.parse(data)
-        console.log('join ' + x.room)
-        // console.log('join ' + uuidv4())
-        socket.join(x.room);
-        socket.to(x.room).emit('joined'); 
+        var join_data = JSON.parse(data)
+        socket.join(join_data.room);
+        socket.to(join_data.room).emit('joined'); 
 
         socket.on('disconnect', () => {
-            console.log(x.userId + 'disconnect ' + x.room)
-            socket.to(x.room).emit('disconnected', x.userId);
-            // var x = JSON.parse(data)
-            // socket.to(x.msg).broadcast.emit('user-disconnected', x.msg)
+            socket.to(join_data.room).emit('disconnected', join_data.userId);
         })
+    });
 
-    });
     socket.on('offer', (offer) => {
-        var x = JSON.parse(offer)
-        console.log('offer ' + offer)
-        socket.to(x.room).emit('offer', offer);
+        var offer_data = JSON.parse(offer)
+        socket.to(offer_data.room).emit('offer', offer);
     });
+
     socket.on('answer', (answer) => {
-        var x = JSON.parse(answer)
-        console.log('answer ' + answer)
-        socket.to(x.room).emit('answer', answer);
+        var answer_data = JSON.parse(answer)
+        socket.to(answer_data.room).emit('answer', answer);
     });
+
     socket.on('ice', (ice) => {
-        var x = JSON.parse(ice)
-        console.log('ice ' + ice)
-        socket.to(x.room).emit('ice', ice);
+        var ice_data = JSON.parse(ice)
+        socket.to(ice_data.room).emit('ice', ice);
     });
 
 });
-
 
 server.listen(PORT, () => {
     console.log('listening on *:3000');
